f3c5dfbca3d81fef1a8821a12ceb7828b847706d
Juan Luis Cano Rodríguez <juanlu@orchest.io>
2022-11-01 11:01:00 +0000

    Tweaks

--- .orchest/environments/fa87a1fd-af2e-44a9-9ea2-3da408cc4b3a/setup_script.sh
+++ .orchest/environments/fa87a1fd-af2e-44a9-9ea2-3da408cc4b3a/setup_script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo apt-get update && sudo -E apt-get install -y --no-install-recommends unzip
-
-mamba install -c conda-forge -y python-graphviz
+sudo apt-get update && sudo -E apt-get install -y --no-install-recommends unzip graphviz
 
 pip install -r requirements.txt
